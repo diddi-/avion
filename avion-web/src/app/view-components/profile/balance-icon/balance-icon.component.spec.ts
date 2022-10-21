7245e70b72af4c009f4c1914cf7da45cf5fecf08
Diddi Oscarsson <diddi.oscarsson@telenor.se>
2022-10-21 16:13:28 +0200

    Add BalanceIconComponent tests

--- avion-web/src/app/view-components/profile/balance-icon/balance-icon.component.spec.ts
+++ avion-web/src/app/view-components/profile/balance-icon/balance-icon.component.spec.ts
@@ -1,23 +1,68 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BalanceIconComponent } from './balance-icon.component';
+import {BalanceIconComponent} from './balance-icon.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MaterialModule} from "@app/material.module";
+import {Subject} from "rxjs";
+import {Profile} from "@app/services/profile/model/profile";
+import {ProfileService} from "@app/services/profile/profile.service";
+
+class MockProfileService {
+  profileSwitched$ = new Subject<Profile>();
+  currentProfile: Profile | undefined = {
+    "firstname": "John",
+    "lastname": "Doe",
+    "balance": 0,
+    "id": 1
+  }
+}
 
 describe('BalanceIconComponent', () => {
   let component: BalanceIconComponent;
   let fixture: ComponentFixture<BalanceIconComponent>;
+  let profileService: MockProfileService;
+  let balanceComponent: BalanceIconComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BalanceIconComponent ]
+      imports: [HttpClientTestingModule, MaterialModule],
+      declarations: [BalanceIconComponent],
+      providers: [
+        BalanceIconComponent,
+        {provide: ProfileService, useClass: MockProfileService}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(BalanceIconComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    balanceComponent = TestBed.inject(BalanceIconComponent);
+    profileService = TestBed.inject(ProfileService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should update balance when profile data is updated", () => {
+    expect(component.balance).toBe(0);
+    const profile: Profile = {
+      "firstname": "John",
+      "lastname": "Doe",
+      "balance": 1500,
+      "id": 1
+    };
+    profileService.profileSwitched$.next(profile);
+    expect(component.balance).toBe(profile.balance);
+  });
+
+  it("should read current profile balance on init", () => {
+    const expected_balance = 50000;
+    if(profileService.currentProfile?.balance !== undefined) // TS2779
+      profileService.currentProfile.balance = expected_balance;
+    expect(component.balance).toBe(0);
+    component.ngOnInit();
+    expect(component.balance).toBe(expected_balance);
+  })
 });
